63339b3dd5540ce05edece9dea24af938ef86cd8
Agzzel <sterneraxel@protonmail.com>
2024-01-25 09:36:08 +0100

    Copied code from local storage to repo

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,64 @@
+import java.util.*;
+
 public class Graph {
+    private final Set<Node> nodes = new HashSet<>();
+
+    public void add(Node node) {
+        nodes.add(node);
+    }
+
+    public Set<Node> getNodes() {
+        return nodes;
+    }
+
+    public static Graph dijkstra(Graph graph, Node startNode){
+        startNode.setDistance(0);
+        Set<Node> visited = new HashSet<>();
+        Set<Node> unvisited = new HashSet<>();
+
+        unvisited.add(startNode);
+
+        while(unvisited.size() != 0){// så länge det finns obesökta noder
+            Node currentNode = getNodeWithLowestDistance(unvisited);
+            unvisited.remove(currentNode);
+
+            //för varje granne (nod med vikt) till current
+            for(Map.Entry<Node,Integer> adjacencyPair : currentNode.getAdjacentNodes().entrySet()){
+                Node adjacentNode = adjacencyPair.getKey();
+                Integer edgeWeight = adjacencyPair.getValue();
+
+                if(!visited.contains(adjacentNode)){
+                    calculateDistance(adjacentNode,edgeWeight,currentNode);
+                    unvisited.add(adjacentNode);
+                    //denna grann-nod läggs in i unvisited och kommer besökas senare
+                }
+            }
+            visited.add(currentNode);
+        }
+        return graph;
+    }
+
+    private static Node getNodeWithLowestDistance(Set<Node> unvisitedNodes){
+        Node lowestDistanceNode = null; // noden med den lägsta distansen från start är okänd
+        int lowestDistance = Integer.MAX_VALUE;
+        for(Node n : unvisitedNodes){
+            int nodeDistance = n.getDistance();
+            if(nodeDistance < lowestDistance){
+                lowestDistance = nodeDistance;
+                lowestDistanceNode = n;
+            }
+        }
+        return lowestDistanceNode;
+    }
+
+    private static void calculateDistance(Node evaluationNode, Integer edgeWeight, Node sourceNode){
+        Integer sourceDistance = sourceNode.getDistance();
+        if(sourceDistance + edgeWeight < evaluationNode.getDistance()){
+            evaluationNode.setDistance(sourceDistance + edgeWeight);
+            LinkedList<Node> shortestPath = new LinkedList<>(sourceNode.getShortestPath());
+            shortestPath.add(sourceNode);
+            evaluationNode.setShortestPath(shortestPath);
+        }
+    }
 
 }
--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,64 @@
+import java.util.*;
+
 public class Node {
+    private final String name;
+
+    private List<Node> shortestPath = new LinkedList<>();
+    private Integer distance = Integer.MAX_VALUE;
+    private Map<Node, Integer> adjacentNodes = new HashMap<>();
+
+    public Node(String n){
+        this.name = n;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void addDestination(Node destination, int distance){
+        adjacentNodes.put(destination,distance);
+    }
+
+    public Integer getDistance(){
+        return distance;
+    }
+
+    public void setDistance(Integer newDistance){
+        this.distance = newDistance;
+    }
+
+    public List<Node> getShortestPath(){
+        return shortestPath;
+    }
+
+    public void setShortestPath(LinkedList<Node> newShortestPath){
+        this.shortestPath = newShortestPath;
+    }
+
+    public Map<Node, Integer> getAdjacentNodes(){
+        return adjacentNodes;
+    }
+
+    public void setAdjacentNodes(Map<Node, Integer> newAdjacentNodes) {
+        this.adjacentNodes = newAdjacentNodes;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return Objects.equals(name, node.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
